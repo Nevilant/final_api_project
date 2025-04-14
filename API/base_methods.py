61c2c3c2de779ea535354b68bea62c826387fcfd
Nevilant <valentin94.94@mail.ru>
2025-04-14 19:29:29 +0300

    Добавлены проверки на статус коды: не 200, 403, 404

--- API/base_methods.py
+++ API/base_methods.py
@@ -1,4 +1,13 @@
+import random
+import string
+
 import allure
+
+
+def random_non_exist_token(length=8):
+    chars = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(chars) for _ in range(length))
+    return random_string
 
 
 class BaseMethods:
@@ -19,6 +28,18 @@ class BaseMethods:
     def check_status_code_is_200(self):
         assert self.response.status_code == 200
 
+    @allure.step("Проверка, что статус код не 200")
+    def check_status_code_is_not_200(self):
+        assert self.response.status_code != 200
+
     @allure.step("Проверка, что статус код 401")
     def check_status_code_is_401(self):
         assert self.response.status_code == 401
+
+    @allure.step("Проверка, что статус код 403")
+    def check_status_code_is_403(self):
+        assert self.response.status_code == 403
+
+    @allure.step("Проверка, что статус код 404")
+    def check_status_code_is_404(self):
+        assert self.response.status_code == 404
