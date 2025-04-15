12fcd563dedf28cf92031189f534a1dc8cf47620
Nevilant <valentin94.94@mail.ru>
2025-04-15 12:34:58 +0300

    Добавлена фикстура для обращения к айдишникам. Переработана фикстура для обращения к рандомному айдишнику

--- conftest.py
+++ conftest.py
@@ -26,10 +26,14 @@ def wrapper_meme():
 
 
 @pytest.fixture()
-def get_random_id_meme(wrapper_meme, get_token):
+def get_list_of_memes_id(wrapper_meme, get_token):
     json_data = wrapper_meme.get_all_memes(get_token)
     response_json = json_data.json()
     memes_list = response_json['data']
     all_ids = [meme['id'] for meme in memes_list]
-    id_meme = choice(all_ids)
-    return id_meme
+    return all_ids
+
+
+@pytest.fixture()
+def get_random_id_meme(get_list_of_memes_id):
+    return choice(get_list_of_memes_id)
